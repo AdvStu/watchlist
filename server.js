74ee9f09cace46c8550611f0d566fb9a42798e45
Advstu <132050051+Advstu@users.noreply.github.com>
2023-05-10 14:43:27 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,14 +1,51 @@
-const http = require('http');
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
 
-const hostname = 'localhost';
-const port = 3000;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello, World!\n');
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/anpr', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
+// Define a schema for the watchlist collection
+const WatchlistSchema = new mongoose.Schema({
+  tenantId: String,
+  licensePlate: String
+});
+
+// Define a model for the watchlist collection
+const WatchlistModel = mongoose.model('Watchlist', WatchlistSchema);
+
+const app = express();
+
+// Parse incoming request bodies in a middleware before your handlers
+app.use(bodyParser.json());
+
+// Create a new watchlist for a tenant
+app.post('/api/watchlist', async (req, res) => {
+  const { tenantId, licensePlate } = req.body;
+  const watchlist = new WatchlistModel({ tenantId, licensePlate });
+  try {
+    await watchlist.save();
+    res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
+});
+
+// Retrieve all watchlists for a tenant
+app.get('/api/watchlist/:tenantId', async (req, res) => {
+  const tenantId = req.params.tenantId;
+  try {
+    const watchlist = await WatchlistModel.find({ tenantId });
+    res.json(watchlist);
+  } catch (error) {
+    res.json({ success: false, error: error.message });
+  }
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
